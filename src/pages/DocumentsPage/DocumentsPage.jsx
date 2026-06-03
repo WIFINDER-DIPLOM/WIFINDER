9942d26346b51f340dbb02d893477cc3dfa3bd3d
Git-art-dark <vladimirrumancev27544@gmail.com>
2026-06-03 10:48:29 +0000

    work at docs

--- src/pages/DocumentsPage/DocumentsPage.jsx
+++ src/pages/DocumentsPage/DocumentsPage.jsx
@@ -1,6 +1,13 @@
+import styles from "./DocumentsPage.module.css"
+
 const DocumentsPage = () => {
   return (
     <>
+      <div className={styles["btn-back"]}onClick={() => window.history.back()}>
+        <svg viewBox="0 0 24 24">
+          <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+        </svg>
+      </div>
       <div>
         <p>Hello, DocumentsPage</p>
       </div>
